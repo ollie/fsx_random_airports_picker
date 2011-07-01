cc6c6c9545edc2bd8f7c176e3c65021ec2465e0f
Oldrich Vetesnik <ut.ollie@seznam.cz>
2011-07-01 15:56:34 +0200

    Added highlight option

--- random.rb
+++ random.rb
@@ -5,10 +5,11 @@ opts = Trollop.options do
 	opt :international, 'International? (default: false)', :default => false, :short => :i, :type => :bool
 	opt :count, 'How many picks do you want?', :default => 5, :short => :c, :type => :int
 	opt :brief, "Don't show link to a map (default: false)", :default => false, :short => :b, :type => :bool
+	opt :highlight, 'Highlight airport codes (default: false)', :default => false, :short => :H, :type => :bool
 end
 
 class Randomizer
-	attr_accessor :international, :count, :brief
+	attr_accessor :international, :count, :brief, :highlight
 
 	def initialize( file_name )
 		@data = JSON.parse File.read( 'codes.txt' )
@@ -26,7 +27,11 @@ class Randomizer
 		code_two	= pick!
 
 		code_info		= "#{ code_one.rjust 4 }-#{ code_two.ljust 4 }"
-		map_info		= "http://www.gcmap.com/mapui?P=\033[1m#{ code_one }-#{ code_two }\033[0m".ljust 46
+		if @highlight
+			map_info		= "http://www.gcmap.com/mapui?P=\033[1m#{ code_one }-#{ code_two }\033[0m".ljust 46
+		else
+			map_info		= "http://www.gcmap.com/mapui?P=#{ code_one }-#{ code_two }".ljust 38
+		end
 		country_info	= country_one == country_two ? country_one : "#{ country_one } -> #{ country_two }"
 
 		puts @brief ? "#{ code_info } (#{ country_info })" : "#{ map_info } (#{ country_info })"
@@ -55,6 +60,7 @@ r = Randomizer.new 'codes.txt'
 r.international = opts[:international]
 r.count = opts[:count]
 r.brief = opts[:brief]
+r.highlight = opts[:highlight]
 
 r.count.times do
 	r.pick_two
